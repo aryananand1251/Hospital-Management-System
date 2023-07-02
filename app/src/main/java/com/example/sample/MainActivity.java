1af8d25e6e964a8365d08f67b05c60609cf3aa0a
aryan <aryananand12517@gmail.com>
2023-07-02 15:45:53 +0530

    stage 2

--- app/src/main/java/com/example/sample/MainActivity.java
+++ app/src/main/java/com/example/sample/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.sample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText txtlogin=findViewById(R.id.txtlogin);
+        final EditText txtpassword=findViewById(R.id.txtpassword);
+        final Button Signup=findViewById(R.id.Signup);
+
+        Signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Register.class));
+            }
+        });
     }
 }
